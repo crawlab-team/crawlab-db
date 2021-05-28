f3c00e3aa957924409711d0559fb3dafec05d02f
marvzhang <tikazyq@163.com>
2021-05-28 09:51:05 +0800

    added transaction

---
+++ mongo/transaction.go
@@ -0,0 +1,29 @@
+package mongo
+
+import (
+	"context"
+	"github.com/crawlab-team/go-trace"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func RunTransaction(fn func(mongo.SessionContext) error) (err error) {
+	s, err := Client.StartSession()
+	if err != nil {
+		return trace.TraceError(err)
+	}
+	if err := s.StartTransaction(); err != nil {
+		return trace.TraceError(err)
+	}
+	if err := mongo.WithSession(context.Background(), s, func(sc mongo.SessionContext) error {
+		if err := fn(sc); err != nil {
+			return trace.TraceError(err)
+		}
+		if err = s.CommitTransaction(sc); err != nil {
+			return trace.TraceError(err)
+		}
+		return nil
+	}); err != nil {
+		return trace.TraceError(err)
+	}
+	return nil
+}
